5b6c273d11a6dee55ff95683a4b9c711c14f0763
simransdsu <simran5590@gmail.com>
2022-04-27 18:12:22 +0530

    Initial Commit

---
+++ RealmApp/RealmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmAppApp.swift
+//  RealmApp
+//
+//  Created by Simran Preet Singh Narang on 2022-04-27.
+//
+
+import SwiftUI
+
+@main
+struct RealmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
